c2d9f1d66b114c5b5a088a86acd05d1571c8ccb6
Glauber Peterson <Petersonglauber@hotmail.com>
2023-03-23 23:46:29 -0300

    Iniciando Desafio Dragao

---
+++ src/aplication/Program.java
@@ -0,0 +1,28 @@
+package aplication;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner ler = new Scanner(System.in);
+        int casos, poderDeLuta;
+        System.out.println("Digite o numero de casos");
+        casos = ler.nextInt();
+
+        for(int i = 0; i < casos; i++){
+            poderDeLuta = ler.nextInt();
+
+//TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
+
+            if(   poderDeLuta <=8000    ){
+                System.out.println(  "Inseto!"      );
+            } else {
+                System.out.println( "Mais de 8000!"        );
+            }
+        }
+}
+	}
+
+
