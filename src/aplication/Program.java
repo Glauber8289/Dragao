cdcf912945763cba8acd4fc1e6b5b4e3152e8720
Glauber Peterson <107453279+Glauber8289@users.noreply.github.com>
2023-05-05 20:18:19 -0300

    Atualizar o Program.java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -22,7 +22,8 @@ public class Program {
                      System.out.println(  "Mais de 8000!"   );
 		    
 
-		    
+		 System.out.println(  "Mais de 8000!"   );
+		       
 		     
 		   
                      
